5ce143fa290bc4e05fe248fe8e526c42d369824a
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2021-02-11 19:48:38 +0100

    -Implementing API endpoints

---
+++ src/Actio.Api/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actio.Api.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() => Content("Hello from Actio API!");
+    }
+}
