6104c5ad404d354131cbbc3e8d56efe6d693fa2f
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2021-02-23 20:19:03 +0100

    -Defining user entity

---
+++ src/Actio.Services.Identity/Domain/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using Actio.Common.Exceptions;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actio.Services.Identity.Domain.Models
+{
+    public class User
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        [BsonElement("email")]
+        public string Email { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("password")]
+        public string Password { get; set; }
+        [BsonElement("salt")]
+        public string Salt { get; set; }
+        [BsonElement("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        protected User()
+        {
+
+        }
+
+        public User(string email, string name)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new ActioException("empty_user_email", $"User email can not be empty.");
+            }
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ActioException("empty_user_name", $"User name can not be empty.");
+            }
+
+            Id = Guid.NewGuid();
+            Email = email.ToLowerInvariant();
+            Name = name;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
