be541169ff0ca0a7cc0841c328f743142aea300a
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2021-03-01 20:39:40 +0100

    -Implementing JWT with HMAC

---
+++ src/Actio.Common/Auth/JsonWebToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actio.Common.Auth
+{
+    public class JsonWebToken
+    {
+        public string Token { get; set; }
+        /// <summary>
+        /// Tells client for how long the token is available
+        /// </summary>
+        public long Expires { get; set; }
+    }
+}
---
+++ src/Actio.Common/Auth/JwtOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actio.Common.Auth
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; set; }
+        public int ExpiryMinutes { get; set; }
+        /// <summary>
+        /// Which service created the token
+        /// </summary>
+        public string Issuer { get; set; }
+    }
+}
