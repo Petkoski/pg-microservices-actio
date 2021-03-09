8fca0534a98f497b02839d4ff5efc3010ae0b15f
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2021-03-09 19:43:15 +0100

    -Refactoring endpoints

--- src/Actio.Api/Controllers/ActivitiesController.cs
+++ src/Actio.Api/Controllers/ActivitiesController.cs
@@ -1,4 +1,5 @@
-﻿using Actio.Common.Commands;
+﻿using Actio.Api.Repositories;
+using Actio.Common.Commands;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,13 +12,43 @@ using System.Threading.Tasks;
 namespace Actio.Api.Controllers
 {
     [Route("[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class ActivitiesController : Controller
     {
         private readonly IBusClient _busClient;
+        private readonly IActivityRepository _activityRepository;
 
-        public ActivitiesController(IBusClient busClient)
+        public ActivitiesController(IBusClient busClient,
+            IActivityRepository activityRepository)
         {
             _busClient = busClient;
+            _activityRepository = activityRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var activities = await _activityRepository
+                .BrowseAsync(Guid.Parse(User.Identity.Name));
+
+            return Json(activities.Select(a => new { a.Id, a.Name, a.Category, a.CreatedAt }));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var activity = await _activityRepository
+                .GetAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+            if (activity.UserId != Guid.Parse(User.Identity.Name))
+            {
+                return Unauthorized();
+            }
+
+            return Json(activity);
         }
 
         [HttpPost]
@@ -25,12 +56,10 @@ namespace Actio.Api.Controllers
         {
             command.Id = Guid.NewGuid();
             command.CreatedAt = DateTime.UtcNow;
+            //Recognizing the user (read from the token)
+            command.UserId = Guid.Parse(User.Identity.Name);
             await _busClient.PublishAsync(command);
             return Accepted($"activities/{command.Id}");
         }
-
-        [HttpGet]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public IActionResult Get() => Content("Secured");
     }
 }
