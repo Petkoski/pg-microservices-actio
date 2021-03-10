8f93927c275a660a5261bda6d388c470201c2f36
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2021-03-10 20:09:20 +0100

    -Api testing

---
+++ tests/Actio.Api.Tests/Unit/Controllers/ActivitiesControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using Actio.Api.Controllers;
+using Actio.Api.Repositories;
+using Actio.Common.Commands;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RawRabbit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Actio.Api.Tests.Unit.Controllers
+{
+    public class ActivitiesControllerTests
+    {
+        [Fact]
+        public async Task Activities_Controller_Post_Should_Return_Accepted()
+        {
+            var busClientMock = new Mock<IBusClient>();
+            var activityRepositoryMock = new Mock<IActivityRepository>();
+            var controller = new ActivitiesController(busClientMock.Object, 
+                activityRepositoryMock.Object);
+            var userId = Guid.NewGuid();
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, userId.ToString())
+                    }, "test"))
+                }
+            };
+
+            var command = new CreateActivity()
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId
+            };
+
+            var result = await controller.Post(command);
+
+            var contextResult = result as AcceptedResult;
+            contextResult.Should().NotBeNull();
+            contextResult.Location.Should().BeEquivalentTo($"activities/{command.Id}");
+        }
+    }
+}
---
+++ tests/Actio.Api.Tests/Unit/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Actio.Api.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Actio.Api.Tests.Unit.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Home_Controller_Get_Should_Return_String_Content()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Get();
+
+            var contentResult = result as ContentResult;
+            contentResult.Should().NotBeNull();
+            contentResult.Content.Should().BeEquivalentTo("Hello from Actio API!");
+        }
+    }
+}
